e908ca9d708b8a295da3af53610993f91dda720c
Amrithya.B <amrithyarao1999@gmail.com>
2025-06-19 17:28:41 +0200

    bert lr

--- scbert/finetune_lr.py
+++ scbert/finetune_lr.py
@@ -72,12 +72,11 @@ class SCDataset(Dataset):
         self.label = label
 
     def __getitem__(self, index):
-        rand_start = random.randint(0, self.data.shape[0]-1)
-        full_seq = self.data[rand_start].toarray()[0]
+        full_seq = self.data[index].toarray()[0]
         full_seq[full_seq > (CLASS - 2)] = CLASS - 2
         full_seq = torch.from_numpy(full_seq).long()
         full_seq = torch.cat((full_seq, torch.tensor([0])))
-        return full_seq, self.label[rand_start]
+        return full_seq, self.label[index]
 
     def __len__(self):
         return self.data.shape[0]
