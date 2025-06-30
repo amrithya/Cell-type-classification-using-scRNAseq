4e4a420e3684f9e97382aeb010480d1a578c4e89
Amrithya <amrithyarao1999@gmail.com>
2025-06-30 11:52:12 +0200

    scf finetune

--- scfoundation/model/preprocess.py
+++ scfoundation/model/preprocess.py
@@ -9,7 +9,7 @@ adata = sc.read_h5ad('/data1/data/corpus/scDATA/Zheng68K.h5ad')
 
 X_df = pd.DataFrame(adata.X.toarray(), columns=adata.var_names, index=adata.obs_names)
 
-gene_list_df = pd.read_csv('path_to/OS_scRNA_gene_index.19264.tsv', delimiter='\t')
+gene_list_df = pd.read_csv('OS_scRNA_gene_index.19264.tsv', delimiter='\t')
 gene_list = list(gene_list_df['gene_name'])
 
 
