e9bc136490c76bbf98b8b77b83302c1bcd91f5dd
Amrithya <amrithyarao1999@gmail.com>
2025-06-30 13:31:46 +0200

    scf finetune

--- scfoundation/model/preprocess.py
+++ scfoundation/model/preprocess.py
@@ -2,17 +2,21 @@ import scanpy as sc
 import pandas as pd
 import numpy as np
 from get_embedding import main_gene_selection
+import os
 
-
-adata = sc.read_h5ad('/data1/data/corpus/scDATA/Zheng68K.h5ad')
-
-
-X_df = pd.DataFrame(adata.X.toarray(), columns=adata.var_names, index=adata.obs_names)
-
-gene_list_df = pd.read_csv('OS_scRNA_gene_index.19264.tsv', delimiter='\t')
-gene_list = list(gene_list_df['gene_name'])
-
-
-X_df, to_fill_columns, var = main_gene_selection(X_df, gene_list)
-
-X_df.to_csv('/data1/data/corpus/scDATA/Zheng68K_scf_preprocessed.csv')
+csv_path = '/data1/data/corpus/scDATA/Zheng68K_scf_preprocessed.csv'
+file_exists = os.path.exists(csv_path)
+if file_exists == False:
+    adata = sc.read_h5ad('/data1/data/corpus/scDATA/Zheng68K.h5ad')
+    X_df = pd.DataFrame(adata.X.toarray(), columns=adata.var_names, index=adata.obs_names)
+    gene_list_df = pd.read_csv('OS_scRNA_gene_index.19264.tsv', delimiter='\t')
+    gene_list = list(gene_list_df['gene_name'])
+    X_df, to_fill_columns, var = main_gene_selection(X_df, gene_list)
+    X_df.to_csv(csv_path)
+    df = pd.read_csv(csv_path, nrows=1)
+    column_names = df.columns.tolist()
+    print(column_names)
+else:
+    df = pd.read_csv(csv_path, nrows=1)
+    column_names = df.columns.tolist()
+    print(column_names)
