c500e86e0d607ff11e0073123b5fec9cb5b36843
Amrithya <amrithyarao1999@gmail.com>
2025-06-30 17:37:17 +0200

    bert shap

--- scbert/run_finetune_shap.sh
+++ scbert/run_finetune_shap.sh
@@ -2,21 +2,23 @@
 
 #SBATCH --job-name="finetune_shap"
 #SBATCH --partition=gpu
-#SBATCH --gres=gpu:2
+#SBATCH --gres=gpu:1
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=2
+#SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=3
 #SBATCH --time=24:00:00
 #SBATCH --hint=nomultithread
-#SBATCH --output=results/finetune_shap_%A_%a.out
-#SBATCH --error=results/finetune_shap_%A_%a.err
-
+#SBATCH --output=results/finetune_shap_%A.out
+#SBATCH --error=results/finetune_shap_%A.err
 
 export CUDA_LAUNCH_BLOCKING=1
+export NCCL_DEBUG=INFO
+export NCCL_IB_DISABLE=1
+
 
 MASTER_PORT=$(python -c "import socket; s=socket.socket(); s.bind(('',0)); print(s.getsockname()[1]); s.close()")
 
-poetry run torchrun --nproc_per_node=2 --master_port=$MASTER_PORT finetune_shap.py \
+poetry run torchrun --nproc_per_node=1 --master_port=$MASTER_PORT finetune_shap.py \
     --data_path "/data1/data/corpus/scDATA/Zheng68K.h5ad" \
     --model_path "/data1/data/corpus/scMODEL/panglao_pretrain.pth"
 
