4a2499f57818a0cda2beed5186dbda1329345089
Amrithya <amrithyarao1999@gmail.com>
2025-06-30 17:40:48 +0200

    bert shap

--- scbert/run_finetune_shap.sh
+++ scbert/run_finetune_shap.sh
@@ -2,9 +2,9 @@
 
 #SBATCH --job-name="finetune_shap"
 #SBATCH --partition=gpu
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:2
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks-per-node=2
 #SBATCH --cpus-per-task=3
 #SBATCH --time=24:00:00
 #SBATCH --hint=nomultithread
@@ -18,7 +18,7 @@ export NCCL_IB_DISABLE=1
 
 MASTER_PORT=$(python -c "import socket; s=socket.socket(); s.bind(('',0)); print(s.getsockname()[1]); s.close()")
 
-poetry run torchrun --nproc_per_node=1 --master_port=$MASTER_PORT finetune_shap.py \
+poetry run torchrun --nproc_per_node=2 --master_port=$MASTER_PORT finetune_shap.py \
     --data_path "/data1/data/corpus/scDATA/Zheng68K.h5ad" \
     --model_path "/data1/data/corpus/scMODEL/panglao_pretrain.pth"
 
