8456d6519f48b367392bc60a4bd62da25770d50f
Amrithya <amrithyarao1999@gmail.com>
2025-07-02 17:58:14 +0200

    bert shap

--- scbert/finetune_shap.py
+++ scbert/finetune_shap.py
@@ -11,25 +11,24 @@ import shap
 from tqdm import tqdm
 from sklearn.model_selection import StratifiedShuffleSplit
 
-class Identity(nn.Module):
-    def __init__(self, dropout=0., h_dim=100, out_dim=10):
-        super().__init__()
+class Identity(torch.nn.Module):
+    def __init__(self, dropout = 0., h_dim = 100, out_dim = 10):
+        super(Identity, self).__init__()
         self.conv1 = nn.Conv2d(1, 1, (1, 200))
         self.act = nn.ReLU()
-        conv_output_width = SEQ_LEN - 200 + 1
-        self.fc1 = nn.Linear(in_features=conv_output_width, out_features=512)
+        self.fc1 = nn.Linear(in_features=SEQ_LEN, out_features=512, bias=True)
         self.act1 = nn.ReLU()
         self.dropout1 = nn.Dropout(dropout)
-        self.fc2 = nn.Linear(in_features=512, out_features=h_dim)
+        self.fc2 = nn.Linear(in_features=512, out_features=h_dim, bias=True)
         self.act2 = nn.ReLU()
         self.dropout2 = nn.Dropout(dropout)
-        self.fc3 = nn.Linear(in_features=h_dim, out_features=out_dim)
+        self.fc3 = nn.Linear(in_features=h_dim, out_features=out_dim, bias=True)
 
     def forward(self, x):
-        x = x[:, None, :, :]
+        x = x[:,None,:,:]
         x = self.conv1(x)
         x = self.act(x)
-        x = x.view(x.shape[0], -1)
+        x = x.view(x.shape[0],-1)
         x = self.fc1(x)
         x = self.act1(x)
         x = self.dropout1(x)
