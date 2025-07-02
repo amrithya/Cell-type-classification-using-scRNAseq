f186e9413ef38035acf01b67f89f10a1b5d5fecf
Amrithya <amrithyarao1999@gmail.com>
2025-07-02 18:23:03 +0200

    bert shap

--- scbert/run_finetune_shap.sh
+++ scbert/run_finetune_shap.sh
@@ -18,7 +18,7 @@ export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
 MASTER_PORT=$(python -c "import socket; s=socket.socket(); s.bind(('',0)); print(s.getsockname()[1]); s.close()")
 
-poetry run torchrun --nproc_per_node=2 --master_port=$MASTER_PORT finetune_shap.py \
+python -m torch.distributed.launch --nproc_per_node=2 --master_port=$MASTER_PORT finetune_shap.py
     
 echo "All Done at $(date)!"
 wait
