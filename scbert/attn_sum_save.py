6a536be57226be6df75bc1b1e38cb1ee64148b25
Amrithya <amrithyarao1999@gmail.com>
2025-07-07 14:06:31 +0200

    bert attenr

--- scbert/attn_sum_save.py
+++ scbert/attn_sum_save.py
@@ -1,35 +1,17 @@
 # -*- coding: utf-8 -*-
 import os
-import gc
 import argparse
-import json
-import logging
-import random
-import math
-import random
-from functools import reduce
 import numpy as np
-import pandas as pd
-from scipy import sparse
-import scipy.io as sio
-from sklearn.metrics import accuracy_score, f1_score, confusion_matrix, precision_recall_fscore_support, classification_report
-import torch
-from torch import nn
-from torch.optim import Adam, SGD, AdamW
-from torch.nn import functional as F
-from torch.optim.lr_scheduler import StepLR, CosineAnnealingWarmRestarts, CyclicLR
-
-from performer_pytorch import PerformerLM
 import scanpy as sc
-from utils import *
+import torch
+from performer_pytorch import PerformerLM
 
 parser = argparse.ArgumentParser() 
-parser.add_argument("--bin_num", type=int, default=5, help='Number of bins.')
-parser.add_argument("--gene_num", type=int, default=16906, help='Number of genes.')
-parser.add_argument("--data_path", type=str, default='./data/data.h5ad', help='Path of data for generating the embeddings.')
-parser.add_argument("--model_path", type=str, default='./model.pth', help='Path of model training on the data.')
-parser.add_argument("--save_dir", type=str, default='./attention/', help='Directory of embeddings to save.')
-
+parser.add_argument("--bin_num", type=int, default=5)
+parser.add_argument("--gene_num", type=int, default=16906)
+parser.add_argument("--data_path", type=str, default='./data/data.h5ad')
+parser.add_argument("--model_path", type=str, default='./model.pth')
+parser.add_argument("--save_dir", type=str, default='./attention/')
 args = parser.parse_args()
 
 SEQ_LEN = args.gene_num + 1
@@ -49,7 +31,6 @@ cellinds = list(set(index_labels.tolist()))
 label_dict, label = np.unique(np.array(data.obs['celltype']), return_inverse=True)
 data_counts = data.X
 
-all_mtx = []
 for cellind in cellinds:
     print(cellind)
     mask = (index_labels == cellind).to_numpy()
@@ -70,17 +51,22 @@ for cellind in cellinds:
     ckpt_path = model_dir
     try:
         ckpt = torch.load(ckpt_path, map_location='cpu')
-        model.load_state_dict(ckpt['model_state_dict'])
+        state_dict = ckpt['model_state_dict']
+        ignored_keys = [k for k in state_dict.keys() if k.startswith("to_out.") or k.startswith("pos_emb.")]
+        for k in ignored_keys:
+            del state_dict[k]
+
+        model.load_state_dict(state_dict, strict=False)
     except Exception as e:
         print(f"[ERROR] Failed to load checkpoint: {e}")
         raise
-    
+
     model = model.to(device)
     print('            =======  Predict start  ======= \n')
+
     batch_size = data_alpha.shape[0]
     model.eval()
     with torch.no_grad():
-
         final_mtx = torch.zeros(batch_size, data_alpha.shape[1]+1).to(device)
         for index in range(batch_size):
             full_seq = data_alpha[index].toarray()[0]
@@ -89,10 +75,11 @@ for cellind in cellinds:
             full_seq = torch.cat((full_seq, torch.tensor([0]))).to(device)
             full_seq = full_seq.unsqueeze(0)
             _, attn_map = model(full_seq, output_attentions=True)
-            attn_map = attn_map.mean((0,1,2))   # mean by batch,head,len
+            attn_map = attn_map.mean((0,1,2))
             attn_map /= attn_map.sum()
             final_mtx[index] = attn_map
 
     final_mtx = final_mtx.detach().cpu().numpy()
-    np.save(os.path.join(save_dir, 'full_attn_sum_{}.npy'.format(cellind)), final_mtx)
+    os.makedirs(save_dir, exist_ok=True)
+    np.save(os.path.join(save_dir, f'full_attn_sum_{cellind}.npy'), final_mtx)
     print(f'            =======  Predict end  ======= \n')
