331eb4ab726cd04c7abe2ec3fead7b0e62ace243
Amrithya <amrithyarao1999@gmail.com>
2025-07-07 12:08:57 +0200

    bert attenr

--- scbert/finetune_attenr.py
+++ scbert/finetune_attenr.py
@@ -124,6 +124,8 @@ try:
     class Identity(nn.Module):
         def __init__(self, dropout=0., h_dim=100, out_dim=10):
             super().__init__()
+            self.conv1 = nn.Conv2d(1, 1, (1, 200))
+            self.act = nn.ReLU()
             self.fc1 = nn.Linear(in_features=SEQ_LEN, out_features=512)
             self.act1 = nn.ReLU()
             self.dropout1 = nn.Dropout(dropout)
@@ -133,9 +135,10 @@ try:
             self.fc3 = nn.Linear(in_features=h_dim, out_features=out_dim)
 
         def forward(self, x):
-            x = self.to_out(x)
-            x = x.permute(0, 2, 1)
-            x = x.reshape(-1, 16907)
+            x = x[:, None, :, :]
+            x = self.conv1(x)
+            x = self.act(x)
+            x = x.view(x.shape[0], -1)
             x = self.fc1(x)
             x = self.act1(x)
             x = self.dropout1(x)
--- scbert/finetune_gradr.py
+++ scbert/finetune_gradr.py
@@ -107,11 +107,11 @@ try:
     )
 
     class Identity(nn.Module):
-        def __init__(self, dropout=0., h_dim=100, out_dim=10, seq_len=16907):
+        def __init__(self, dropout=0., h_dim=100, out_dim=10):
             super().__init__()
             self.conv1 = nn.Conv2d(1, 1, (1, 200))
             self.act = nn.ReLU()
-            self.fc1 = nn.Linear(in_features=seq_len, out_features=512)
+            self.fc1 = nn.Linear(in_features=SEQ_LEN, out_features=512)
             self.act1 = nn.ReLU()
             self.dropout1 = nn.Dropout(dropout)
             self.fc2 = nn.Linear(in_features=512, out_features=h_dim)
