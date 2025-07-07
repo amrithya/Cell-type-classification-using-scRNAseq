2b6cc072d4ea5c88ee1bab9b8a43cc3b4c9ac67e
Amrithya <amrithyarao1999@gmail.com>
2025-07-07 12:51:02 +0200

    bert attenr

--- scbert/finetune_attenr.py
+++ scbert/finetune_attenr.py
@@ -124,9 +124,7 @@ try:
     class Identity(nn.Module):
         def __init__(self, dropout=0., h_dim=100, out_dim=10):
             super().__init__()
-            self.conv1 = nn.Conv2d(1, 1, (1, 200))
-            self.act = nn.ReLU()
-            self.fc1 = nn.Linear(in_features=SEQ_LEN, out_features=512)
+            self.fc1 = nn.Linear(in_features=200, out_features=512)
             self.act1 = nn.ReLU()
             self.dropout1 = nn.Dropout(dropout)
             self.fc2 = nn.Linear(in_features=512, out_features=h_dim)
@@ -135,10 +133,6 @@ try:
             self.fc3 = nn.Linear(in_features=h_dim, out_features=out_dim)
 
         def forward(self, x):
-            x = x[:, None, :, :]
-            x = self.conv1(x)
-            x = self.act(x)
-            x = x.view(x.shape[0], -1)
             x = self.fc1(x)
             x = self.act1(x)
             x = self.dropout1(x)
