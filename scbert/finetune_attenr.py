6bdee6cbe6f6241c1caea31e2443f282e3e77fb6
Amrithya <amrithyarao1999@gmail.com>
2025-07-07 11:41:24 +0200

    bert attenr

--- scbert/finetune_attenr.py
+++ scbert/finetune_attenr.py
@@ -135,7 +135,8 @@ try:
             self.fc3 = nn.Linear(in_features=h_dim, out_features=out_dim)
 
         def forward(self, x):
-            x = x[:, None, :, :]
+            if x.dim() == 3:
+                x = x.unsqueeze(1)
             x = self.conv1(x)
             x = self.act(x)
             x = x.view(x.shape[0], -1)
