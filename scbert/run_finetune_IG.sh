035dbd115af08646354cbb60f85da29b25da1c8a
Amrithya <amrithyarao1999@gmail.com>
2025-07-09 17:00:29 +0200

    bert IG

--- scbert/run_finetune_IG.sh
+++ scbert/run_finetune_IG.sh
@@ -8,15 +8,15 @@
 #SBATCH --cpus-per-task=3
 #SBATCH --time=48:00:00
 #SBATCH --hint=nomultithread
-#SBATCH --output=results/finetune_deeplift_%A_%a.out
-#SBATCH --error=results/finetune_deeplift_%A_%a.err
+#SBATCH --output=results/finetune_IG_%A_%a.out
+#SBATCH --error=results/finetune_IG_%A_%a.err
 #SBATCH --array=1
 
 MASTER_PORT=$(python -c "import socket; s=socket.socket(); s.bind(('',0)); print(s.getsockname()[1]); s.close()")
 
 export CUDA_LAUNCH_BLOCKING=1
 
-poetry run python -u -m torch.distributed.launch --nproc_per_node=2 --master_port=$MASTER_PORT finetune_deeplift.py \
+poetry run python -u -m torch.distributed.launch --nproc_per_node=2 --master_port=$MASTER_PORT finetune_IG.py \
     --data_path "/data1/data/corpus/scDATA/Zheng68K.h5ad" \
     --model_path "/data1/data/corpus/scMODEL/panglao_pretrain.pth"
 
