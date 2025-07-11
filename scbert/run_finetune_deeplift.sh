48930afef6e92756622080cf9f26474db2c7d5ca
Amrithya <amrithyarao1999@gmail.com>
2025-07-11 17:55:42 +0200

    bert deeplift

--- scbert/run_finetune_deeplift.sh
+++ scbert/run_finetune_deeplift.sh
@@ -2,9 +2,9 @@
 
 #SBATCH --job-name="finetune_deeplift"
 #SBATCH --partition=gpu
-#SBATCH --gres=gpu:2
+#SBATCH --gres=gpu:1
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=2
+#SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=3
 #SBATCH --time=48:00:00
 #SBATCH --hint=nomultithread
@@ -12,11 +12,9 @@
 #SBATCH --error=results/finetune_deeplift_%A_%a.err
 #SBATCH --array=1
 
-MASTER_PORT=$(python -c "import socket; s=socket.socket(); s.bind(('',0)); print(s.getsockname()[1]); s.close()")
-
 export CUDA_LAUNCH_BLOCKING=1
 
-poetry run python -u -m torch.distributed.launch --nproc_per_node=2 --master_port=$MASTER_PORT finetune_deeplift.py \
+poetry run python -u finetune_deeplift.py \
     --data_path "/data1/data/corpus/scDATA/Zheng68K.h5ad" \
     --model_path "/data1/data/corpus/scMODEL/panglao_pretrain.pth"
 
