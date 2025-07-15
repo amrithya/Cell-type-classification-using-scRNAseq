1b3ccc93f7573f879848622f9a5ba91b027ab44e
Amrithya <amrithyarao1999@gmail.com>
2025-07-15 09:07:13 +0200

    bert IG

--- scbert/finetune_IG.py
+++ scbert/finetune_IG.py
@@ -31,6 +31,7 @@ dist.init_process_group(backend='nccl')
 rank = dist.get_rank()
 world_size = dist.get_world_size()
 local_rank = args.local_rank
+local_rank = int(os.environ.get("LOCAL_RANK", 0))
 is_master = rank == 0
 
 torch.cuda.set_device(local_rank)
