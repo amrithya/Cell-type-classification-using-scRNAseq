c63266651f88be45fc1128d900d0a8978805dfa5
Amrithya <amrithyarao1999@gmail.com>
2025-07-15 13:26:52 +0200

    bert cancer

--- scbert/utils.py
+++ scbert/utils.py
@@ -69,7 +69,7 @@ def save_ckpt(epoch, model, optimizer, scheduler, losses, model_name):
     """
     Save checkpoint including epoch, model, optimizer, scheduler states and losses.
     """
-    save_path = f"/data1/data/corpus/scMODEL/{model_name}_full_model_Zheng68K.pkl"
+    save_path = f"/data1/data/corpus/scMODEL/{model_name}_full_model_cancer.pkl"
     ckpt_dir = os.path.dirname(save_path)
     if not os.path.exists(ckpt_dir):
         os.makedirs(ckpt_dir)
