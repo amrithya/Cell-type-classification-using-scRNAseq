38fc835f1f617801734973c99d37d27d4aff20d2
Amrithya <amrithyarao1999@gmail.com>
2025-07-15 14:02:09 +0200

    bert cancer

--- scbert/finetune_cancer.py
+++ scbert/finetune_cancer.py
@@ -100,25 +100,15 @@ class Identity(torch.nn.Module):
         super(Identity, self).__init__()
         self.conv1 = nn.Conv2d(1, 1, (1, 200))
         self.act = nn.ReLU()
-        self.fc1 = nn.Linear(in_features=SEQ_LEN, out_features=512, bias=True)
-        self.act1 = nn.ReLU()
-        self.dropout1 = nn.Dropout(dropout)
-        self.fc2 = nn.Linear(in_features=512, out_features=h_dim, bias=True)
-        self.act2 = nn.ReLU()
-        self.dropout2 = nn.Dropout(dropout)
-        self.fc3 = nn.Linear(in_features=h_dim, out_features=out_dim, bias=True)
+        
+        self.fc3 = nn.Linear(in_features=SEQ_LEN, out_features=out_dim, bias=True)
 
     def forward(self, x):
         x = x[:,None,:,:]
         x = self.conv1(x)
         x = self.act(x)
         x = x.view(x.shape[0],-1)
-        x = self.fc1(x)
-        x = self.act1(x)
-        x = self.dropout1(x)
-        x = self.fc2(x)
-        x = self.act2(x)
-        x = self.dropout2(x)
+       
         x = self.fc3(x)
         return x
 
