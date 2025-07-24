83dc076dcf24508f2ca22493b20926ea8f849f25
Amrithya.B <amrithyarao1999@gmail.com>
2025-07-24 16:40:08 +0200

    kidney scf

--- scfoundation/model/get_embedding.py
+++ scfoundation/model/get_embedding.py
@@ -134,7 +134,7 @@ def main():
 
     geneexpemb=[]
     batchcontainer = []
-    strname = os.path.join(args.save_path, args.task_name +'_'+ args.ckpt_name +"_"+ args.input_type + '_' + args.output_type + '_embedding_' + args.tgthighres + '_resolution.npy')
+    strname = "/data1/data/corpus/scDATA/scfoundation/mt_kidney_rcc_cca_paa_2_scf.npy"
     print('save at {}'.format(strname))
     
     #Inference
